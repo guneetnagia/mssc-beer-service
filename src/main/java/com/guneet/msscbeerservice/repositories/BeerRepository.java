5971024c7798a204ea8019f8658e14fba6d065fd
Guneet Singh <guneetnagia24@gmail.com>
2020-07-12 22:03:50 +0530

    Junit 5 Controller

---
+++ src/main/java/com/guneet/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,21 @@
+package com.guneet.msscbeerservice.repositories;
+
+import com.guneet.msscbeerservice.domain.Beer;
+import com.guneet.msscbeerservice.web.model.BeerDto;
+import com.guneet.msscbeerservice.web.model.BeerStyleEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<BeerDto, UUID> {
+
+    Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
+
+    Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
+
+    Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
+
+    Beer findByUpc(String upc);
+}
---
+++ src/main/java/com/guneet/msscbeerservice/services/BeerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.guneet.msscbeerservice.services;
+
+import com.guneet.msscbeerservice.web.model.BeerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BeerServiceImpl implements BeerService{
+
+    @Override
+    public BeerDto getBeerById(UUID beerId) {
+        return null;
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return null;
+    }
+
+    @Override
+    public BeerDto updateBeer(BeerDto beerDto) {
+        return null;
+    }
+}
