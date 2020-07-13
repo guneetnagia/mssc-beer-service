c6cd154b829d1c5091ced4a7536ce0f3b1876d31
Guneet Singh <guneetnagia24@gmail.com>
2020-07-13 23:23:17 +0530

    MapStruct dependency

--- src/main/java/com/guneet/msscbeerservice/services/BeerServiceImpl.java
+++ src/main/java/com/guneet/msscbeerservice/services/BeerServiceImpl.java
@@ -1,12 +1,17 @@
 package com.guneet.msscbeerservice.services;
 
+import com.guneet.msscbeerservice.repositories.BeerRepository;
 import com.guneet.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @Service
 public class BeerServiceImpl implements BeerService{
+
+    private final BeerRepository beerRepository;
 
     @Override
     public BeerDto getBeerById(UUID beerId) {
